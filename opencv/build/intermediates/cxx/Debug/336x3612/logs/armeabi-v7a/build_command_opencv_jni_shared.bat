bededcfc000ade9e69830d0b1b068a466df0a5be
nitrojenFaruk <seffaa40@gmail.com>
2023-07-03 22:43:42 +0300

    First Commit

---
+++ opencv/build/intermediates/cxx/Debug/336x3612/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\seffa\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\seffa\\StudioProjects\\GlassicoApp\\opencv\\.cxx\\Debug\\336x3612\\armeabi-v7a" ^
+  opencv_jni_shared
---
+++ opencv/build/intermediates/cxx/Debug/336x3612/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\seffa\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\seffa\\StudioProjects\\GlassicoApp\\opencv\\.cxx\\Debug\\336x3612\\x86_64" ^
+  opencv_jni_shared
